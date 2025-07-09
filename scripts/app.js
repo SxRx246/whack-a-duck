9f0035b83b47b4dfcf6e48731bae16d8e124da78
Sara Alqallaf <202107125@stu.uob.edu.bh>
2025-07-09 11:17:19 +0300

    update the codes

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,23 @@
 function init(){
 
+    const gridElem = document.querySelector('.grid')
+    function createGrid(){
+        // for every call that we require create a div
+        // append this call to our grid
+
+    //    running the loop 100 times, then create div 100 rimes and add them to the grid class
+        for (let i=0 ; i<100 ; i++){
+            // thats will create 100 div
+            const cell = document.createElement('div')
+            cell.textContent = i
+
+            // textContent is the text content of the cell
+            // it is the same as the innerText
+
+            gridElem.appendChild(cell)
+        }
+    }
+    createGrid()
+
 }
 document.addEventListener("DOMContentLoaded",init)
